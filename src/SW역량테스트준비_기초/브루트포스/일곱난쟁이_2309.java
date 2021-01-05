58ff7999a20bb92d8837426af253d82ec7fd89f9
youngwoo311 <devlee311@gmail.com>
2021-01-06 00:02:12 +0900

    refactor

--- src/SW역량테스트준비_기초/브루트포스/일곱난쟁이_2309.java
+++ src/SW역량테스트준비_기초/브루트포스/일곱난쟁이_2309.java
@@ -1,7 +1,7 @@
 package SW역량테스트준비_기초.브루트포스;
 
 public class 일곱난쟁이_2309 {
-    
+
     public static void main(String[] args) {
 
     }
---
+++ src/SW역량테스트준비_연습/브루트포스/리모컨_1107.java
@@ -0,0 +1,8 @@
+package SW역량테스트준비_연습.브루트포스;
+
+public class 리모컨_1107 {
+
+    public static void main(String[] args) {
+        
+    }
+}
